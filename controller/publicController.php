f18a830658b0049d024d44e0e64bf5575a8c7090
leerlandais <leerlandais@gmail.com>
2024-08-28 20:51:58 +0100

    fixed and online

--- controller/publicController.php
+++ controller/publicController.php
@@ -34,6 +34,7 @@ switch ($route) {
     case 'select':
        if(isset($_GET["type"])) {
            $type = htmlspecialchars(strip_tags(trim($_GET["type"])));
+           $getData = null;
         switch ($type) {
             case 'call':
                     $headerTitle = "PHP Calls";
@@ -52,9 +53,9 @@ switch ($route) {
                     $getData = $codeManager->getDataByType("jsXtra");
                 break;
             case 'unix' :
-                    $headerTitle = "Linux";
-                //    $getData = $unixManager->getDataByType("unix");
-            echo $twig->render('publicView/public.view.unix.twig');
+                      $headerTitle = "Linux";
+                $getData = $codeManager->getDataByType("unix");
+                break;
             case 'html':
                     $headerTitle = "HTML";
                     $getData = $htmlManager->getHtml();
@@ -64,7 +65,7 @@ switch ($route) {
                 echo $twig->render('publicView/public.404.html.twig');
                 break;
         }
-                echo $twig->render('publicView/public.view.main.twig', ['getData' => $getData]);
+                echo $twig->render('publicView/public.view.main.twig', ['getData' => $getData, 'headerTitle' => $headerTitle]);
        }
         break;
     case 'showCode':
