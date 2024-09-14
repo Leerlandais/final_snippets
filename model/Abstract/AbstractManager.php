5d1be38f085c5689dca0bc8b9fcd551479cb5c12
leerlandais <leerlandais@gmail.com>
2024-09-14 20:10:04 +0100

    change of tiny line of text - only commited to prevent merge problems

--- model/Abstract/AbstractManager.php
+++ model/Abstract/AbstractManager.php
@@ -1,7 +1,7 @@
 <?php
 
 // Seeming as all Managers need a __construct, it seems logical to move it to an Abstract Class and extend it
-
+// change MyPDO to the necessary
 namespace model\Abstract;
 use model\MyPDO;
 
