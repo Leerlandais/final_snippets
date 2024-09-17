db0e9db7992ae69f5814a32d8dc2c43d43b33bb3
leerlandais <leerlandais@gmail.com>
2024-09-17 11:04:33 +0100

    createUser: added JS test for password match

--- public/scripts/createUserScript.js
+++ public/scripts/createUserScript.js
@@ -1,1 +1,16 @@
 console.log("createUserScript");
+
+const   createLoginPwd = document.getElementById("createLoginPwd"),
+        createLoginPwdX = document.getElementById("createLoginPwdX"),
+        createLoginForm = document.getElementById("createLoginForm");
+
+createLoginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let pwd = createLoginPwd.value,
+        pwdX = createLoginPwdX.value;
+    if (pwd === pwdX) {
+        alert('all good');
+    }else {
+        alert("passwords don't match");
+    }
+})
